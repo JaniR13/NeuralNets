b0ff2f1cfc06e7cf102f30f8c5b08921cc560c95
Janette <janette.rounds@msu.montana.edu>
2015-09-19 11:41:15 -0600

    added more abstract methods

--- NeuralNets/src/runmodels/AbstractModel.java
+++ NeuralNets/src/runmodels/AbstractModel.java
@@ -14,6 +14,10 @@ public abstract class AbstractModel {
     void importData(){
         
     }
+    void exportResults(){
+        
+    }
+    abstract void compileResults();
     abstract void execute();
     
 }
