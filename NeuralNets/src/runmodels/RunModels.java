3717167e4251b32f8cafb55a716aa9a378e42f6a
Janette <janette.rounds@msu.montana.edu>
2015-09-20 14:27:00 -0600

    added comments

--- NeuralNets/src/runmodels/RunModels.java
+++ NeuralNets/src/runmodels/RunModels.java
@@ -15,7 +15,7 @@ public class RunModels {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        AbstractModel x = new NeuralNets();
     }
     
 }
