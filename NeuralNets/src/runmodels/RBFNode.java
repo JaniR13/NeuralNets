c03f383883601d1bdc200c5c6644ce74ed2692e9
Janette <janette.rounds@msu.montana.edu>
2015-10-08 17:50:17 -0600

    Created hidden layer

---
+++ NeuralNets/src/runmodels/RBFNode.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package runmodels;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Janette
+ */
+public class RBFNode {
+    double outweight;
+    double inweight;
+    double var;
+    double mean;
+    public RBFNode(){
+        
+    }
+    public void calculateActivation(ArrayList x){
+        
+    }
+}
