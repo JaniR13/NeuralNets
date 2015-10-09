4c947c194f726c653cceddb0fc2354dd7d5a1d52
Janette <janette.rounds@msu.montana.edu>
2015-10-09 11:08:55 -0600

    Split weight update function into 2 functions

--- NeuralNets/src/runmodels/KernelANN.java
+++ NeuralNets/src/runmodels/KernelANN.java
@@ -1,13 +1,15 @@
 package runmodels;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class KernelANN extends AbstractModel {
 
     private ArrayList<Double> inputs;
     private ArrayList<RBFNode> functions;
     private ArrayList<Double> outputs;
-
+    private double eta = 0.1;//learning rate
+    private Distance dist = new Distance();
     public KernelANN() {
 
     }
@@ -25,23 +27,24 @@ public class KernelANN extends AbstractModel {
     }
 
     public void buildNetwork(int numInputs, int numOutputs, int numFunctions) {
-       // for(int i = 0; i < numInputs; i++){ //TODO need to figure out if inputs means features or training examples...
-           // inputs.set(i, 0.0);
-        //}
+        Random rand = new Random();
+        for(int i = 0; i < numInputs; i++){ //TODO need to figure out if inputs means features or training examples...
+            inputs.set(i, 0.0);
+        }
         for(int j = 0; j < numOutputs; j++){
             outputs.set(j, 0.0);
         }
         for(int k = 0; k < numFunctions; k++){
             RBFNode node = new RBFNode();
             functions.set(k, node);
-            for(int i = 0; i < inputs.size(); i++){
-                functions.get(k).inweights.set(i, 0.0);//initialize to Random weights?
+            for(int i = 0; i < outputs.size(); i++){
+                functions.get(k).outweights.set(i, rand.nextDouble());//initialize to Random weights?
             }
-            for(int j = 0; j < outputs.size(); j++){
-                functions.get(k).inweights.set(j, 0.0);//initialize to Random weights?
-            }
-            functions.get(k).var = 0.0; //TODO set variance
-            //TODO how to set mean?
+//            for(int j = 0; j < outputs.size(); j++){
+//                functions.get(k).inweights.set(j, rand.nextDouble());//initialize to Random weights?
+//            }
+            //TODO set variance
+            //TODO set means via k-means
         }
     }
 
@@ -49,10 +52,14 @@ public class KernelANN extends AbstractModel {
         //TODO figure out how to do k-means...
     }
 
-    public void updateWeights() {
-        //TODO do weight updates
+    public double updateIndWeight(double inweight, double input, ArrayList<Double> target, ArrayList<Double> observed, ArrayList<Double> x) {
+        double outweight = inweight;
+        outweight += dist.calculateDistance(target, observed)*eta*input;//multiply this by input
+        return outweight;
     }
-
+    public void updateAllWeights(){
+        
+    }
     public void generateOutputs() {
         for (int i = 0; i < outputs.size(); i++) {
             //for each output node calculate weighted sum of hidden layers
