13acf3274930a2edcbb2e379c42af10ad18703b2
Janette <janette.rounds@msu.montana.edu>
2015-10-09 13:12:36 -0600

    Planned k-means class

---
+++ NeuralNets/src/runmodels/KMeans.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package runmodels;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Janette
+ */
+public class KMeans {
+    public KMeans(){
+        
+    }
+    public ArrayList<ArrayList> createClusters(int k, String fname){
+        //read data from file 
+        //figure out dimensionality of input and output
+    
+        //Generate k examples of dimensionality same as input
+        //for each example
+            //functions.get(i).means = example
+        //for a number of iterations
+            //for each datapoint
+                //measure distance from point to all examples
+                //assign to closest example cluster
+            //remove example from cluster
+            //find average of points in cluster 
+            //assign new average to cluster
+        return new ArrayList();
+    }
+}
