5510572c6a29670d92c202b171c28fb7efd0ae2c
Janette <janette.rounds@msu.montana.edu>
2015-10-11 17:14:26 -0600

    Added Rollie's things

---
+++ NeuralNets/src/runmodels/Logistic.java
@@ -0,0 +1,29 @@
+package runmodels;
+
+
+public class Logistic extends ActivationFunction{
+
+	private double xo;
+	private double L;
+	private double k;
+	
+	
+	/** 
+	 * Generates a standard logistic function (k=1, xo=0, L=1)
+	 */
+	public Logistic(){
+		// standard is k=1, xo=0, L=1
+		xo = 0;
+		L = 1;
+		k = 1;
+	}
+	
+	public double calcfx(double x){
+		return ((L)/(1 + Math.exp(-1*k*(x-xo))));
+	}
+	
+	public Double partialDeriv(Double neuronOutput){
+		return (neuronOutput * (1-neuronOutput));
+	}
+	
+}
