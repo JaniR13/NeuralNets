fd471527cb2957b8afea820cf0d0df1324918fe7
Janette <janette.rounds@msu.montana.edu>
2015-10-10 21:21:23 -0600

    After some debugging, everything sucks

--- NeuralNets/src/runmodels/KernelANN.java
+++ NeuralNets/src/runmodels/KernelANN.java
@@ -8,13 +8,15 @@ import java.util.Random;
 public class KernelANN extends AbstractModel {
 
     //private ArrayList<Double> inputs;
-    private ArrayList<RBFNode> functions;
-    private ArrayList<Double> outputs;
-    private ArrayList<Double> targets;
+    private ArrayList<RBFNode> functions = new ArrayList();
+    private ArrayList<Double> outputs = new ArrayList();
+    private ArrayList<Double> targets = new ArrayList();
     private double eta = 0.1;//learning rate
     private Distance dist = new Distance();
     private double variance;
-    private double totalError;
+    public double totalError;
+    public int dim2 = 0;
+    public int datasize = 0;
     public KernelANN() {
 
     }
@@ -32,6 +34,7 @@ public class KernelANN extends AbstractModel {
     }
 
     public void buildNetwork(int numInputs, int numOutputs, int numFunctions) {
+        //System.out.println("build networks: in: " + numInputs + ", out: " + numOutputs + ", func: " +numFunctions);
         Random rand = new Random();
 //        for(int i = 0; i < numInputs; i++){ //TODO need to figure out if inputs means features or training examples...
 //            inputs.add(0.0);
@@ -39,11 +42,16 @@ public class KernelANN extends AbstractModel {
         for(int j = 0; j < numOutputs; j++){
             outputs.add(0.0);
         }
-        for(int k = 0; k < numFunctions; k++){
+        for(int m = 0; m < numFunctions; m++){
             RBFNode node = new RBFNode();
-            functions.set(k, node);
+            functions.add(node);
+            //System.out.println(outputs.size());
             for(int i = 0; i < outputs.size(); i++){
-                functions.get(k).outweights.add(rand.nextDouble());//initialize to Random weights
+                //System.out.println(i + " : " + m);
+                //System.out.println(functions.get(m).outweights);
+                double r = rand.nextDouble();
+                //System.out.println(r);
+                functions.get(m).outweights.add(r);//initialize to Random weights
             }
             
         }
@@ -60,13 +68,24 @@ public class KernelANN extends AbstractModel {
 
 		br = new BufferedReader(new FileReader(fname));
 		while ((line = br.readLine()) != null) {
+                    //System.out.println(line + " : " + count);
                     String[] example = line.split(cvsSplitBy);
-                    int n = example.length;
-                    for(int i = 0; i<n-1; i++){
-                        input.get(count).add(Double.parseDouble(example[i]));
+                    dim2 = example.length;
+                    //System.out.println(n-1);
+                    input.add(new ArrayList());
+                    for(int i = 0; i<dim2-1; i++){
+                        //System.out.println(i + " " + count);
+                        int x = Integer.parseInt(example[i]);
+                        //System.out.println(x);
+                        input.get(count).add(x);
                     }
-                    targets.add(Double.parseDouble(example[n-1]));
+                    dim2 = dim2-1;
+                    double x = Double.parseDouble(example[dim2]);
+                    //System.out.println(x);
+                    targets.add(x);
+                    count++;
                 }
+                datasize = count;
         }catch(Exception e){
             e.printStackTrace();
         }
@@ -75,37 +94,52 @@ public class KernelANN extends AbstractModel {
 
     public void trainNetwork(String fname, double var, int k, double threshold){
         ArrayList<ArrayList> input = readData(fname);
+        System.out.println("finished read data");
         kMeansClustering(k, input, 1, var);
-        totalError = Double.MAX_VALUE;
-        int count = 0;
-        while(totalError > 0.00001 && count < Integer.MAX_VALUE){
-            for(int i = 0; i < input.size(); i++){//for each training example
-                for(int j = 0; j < functions.size(); j++){//for each RBF function
+        System.out.println("finished clustering");
+        //System.out.println(Double.MAX_VALUE);
+        totalError = (double)(Integer.MAX_VALUE);
+        int count2 = 0;
+        while(totalError > threshold && count2 < 10){
+            //System.out.println(" run " + count2);
+            System.out.println(count2 + " : " + totalError);
+            for(int i = 0; i < datasize; i++){//for each training example
+                for(int j = 0; j < k; j++){//for each RBF function
+                    //System.out.println("input: " + input.get(i));
+                    System.out.println("Run: " + i + " " + j + " : "+ functions.get(j).calculateActivation(input.get(i), variance));
                     functions.get(j).activationOut = 
                             functions.get(j).calculateActivation(input.get(i), variance);
+                    System.out.println(functions.get(j).activationOut);
                        
                 }
                 generateOutputs();
                 updateAllWeights(targets);
                 ArrayList<Double> tar = new ArrayList();
                 tar.add(targets.get(i));
+                System.out.println("Unupdated error: " + totalError);
                 totalError = calcError(tar, outputs);
+                
+                //System.out.println(totalError + " : " + count2);
             }
+            count2++;
         }
         
     }
     
     public double calcError(ArrayList target, ArrayList out){
-        return dist.calculateDistance(target, out);
+        return dist.calculateDistance(target, out, 1);
     }
     public void kMeansClustering(int k, ArrayList<ArrayList> in, int outDim, double var){//input all examples and k
         KMeans kmeans = new KMeans();
         //ArrayList in = readData(fname);
-        ArrayList<ArrayList> meanslist = kmeans.createClusters(k, in);
-        int inDim = meanslist.get(0).size();
-        buildNetwork(inDim, outDim, k);
+        ArrayList<ArrayList> meanslist = kmeans.createClusters(k, in, dim2);
+        //System.out.println(meanslist);
+        //int inDim = meanslist.get(0).size();
+        buildNetwork(dim2, outDim, k);
         for(int i = 0; i < functions.size(); i++){
+            
             functions.get(i).means.addAll(meanslist.get(i));
+            System.out.println(functions.get(i).means);
         }
         //variance?
         variance = var;    
@@ -113,7 +147,7 @@ public class KernelANN extends AbstractModel {
 
     public double updateIndWeight(double inweight, double input, ArrayList<Double> target, ArrayList<Double> observed) {
         double outweight = inweight;
-        outweight += dist.calculateDistance(target, observed)*eta*input;//multiply this by input
+        outweight += dist.calculateDistance(target, observed, 1)*eta*input;//multiply this by input
         return outweight;
     }
     public void updateAllWeights(ArrayList<Double> target){
