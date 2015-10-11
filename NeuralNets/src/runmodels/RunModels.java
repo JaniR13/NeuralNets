5f56a8829bc1e81f4479afdd192a7ede26093697
Janette <janette.rounds@msu.montana.edu>
2015-10-10 21:21:39 -0600

    After some debugging, everything sucks

--- NeuralNets/src/runmodels/RunModels.java
+++ NeuralNets/src/runmodels/RunModels.java
@@ -15,7 +15,10 @@ public class RunModels {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        AbstractModel x = new NeuralNets();
+        KernelANN x = new KernelANN();
+        String name = "C://Users//Janette//Documents//GradSchoolStuff//MachineLearning//Project2//rosenbrock_size2.txt";
+        x.trainNetwork(name, 0.5, 11, 0.00001);
+        System.out.println("Finished! Error: " + x.totalError );
     }
     
 }
