5d92c84bde8f2787c5abdc9e03a4de3553745eaa
Janette <janette.rounds@msu.montana.edu>
2015-10-11 17:13:37 -0600

    Added Rollie's things

--- NeuralNets/src/runmodels/FeedForwardANNTest.java
+++ NeuralNets/src/runmodels/FeedForwardANNTest.java
@@ -6,58 +6,154 @@ import java.util.ArrayList;
 
 import org.junit.Test;
 
+
 public class FeedForwardANNTest {
 
-
+//	@Test
+//	public void testTrainSingleOutput() {
+//		ArrayList<Double> inputs = new ArrayList<Double>();
+//		inputs.add(3.0);
+//		inputs.add(-0.89);
+//		inputs.add(.25);
+//
+//		ArrayList<Double> expectedOutput = new ArrayList<Double>();
+//		expectedOutput.add(.999);
+//
+//		FeedForwardANN net = new FeedForwardANN(1, 2, inputs, expectedOutput, true, false);
+//
+//		net.train();
+//		net.print();
+//	} 
 	
-	/**@Test
-	public void backPropTestLessThanOne(){
-		FeedForwardANN net = new FeedForwardANN(4);
-		ArrayList<Double> inputs = new ArrayList<Double>();
-		inputs.add(-2.5);
-		inputs.add(2.0);
-		inputs.add(-1.0);
-		inputs.add(3.0);
-		
-		ArrayList<Double> expectedOutputs = new ArrayList<Double>();
-		expectedOutputs.add(.99);
-		expectedOutputs.add(.99);
-		expectedOutputs.add(.99);
-		expectedOutputs.add(.99);
-		
-		net.setExpectedOutputs(expectedOutputs);
-		net.generateOutput(inputs);	
-		net.backProp();
-
-		for (int i = 0; i < 30; i++){
-			net.generateOutput(inputs);	
-			net.backProp();
-		}
-	} */
+//	@Test
+//	public void testTrainSingleOutput2() {
+//		ArrayList<Double> inputs = new ArrayList<Double>();
+//		inputs.add(7.0);
+//		inputs.add(-0.84);
+//		inputs.add(.05);
+//
+//		ArrayList<Double> expectedOutput = new ArrayList<Double>();
+//		expectedOutput.add(.25);
+//
+//		FeedForwardANN net = new FeedForwardANN(1, 2, inputs, expectedOutput, true, false);
+//
+//		net.train();
+//		net.print();
+//	}
+	
+//	@Test
+//	public void testTrainMultOutput() {
+//		ArrayList<Double> inputs = new ArrayList<Double>();
+//		inputs.add(3.0);
+//		inputs.add(-0.89);
+//		inputs.add(.25);
+//
+//		ArrayList<Double> expectedOutput = new ArrayList<Double>();
+//		expectedOutput.add(.972);
+//		expectedOutput.add(.555);
+//
+//		FeedForwardANN net = new FeedForwardANN(2, 10, inputs, expectedOutput, true, false);
+//
+//		net.train();
+//		net.print();
+//	}
+	
+//	@Test
+//	public void testLinearActivationMult(){
+//		ArrayList<Double> inputs = new ArrayList<Double>();
+//		inputs.add(3.0);
+//		inputs.add(-0.89);
+//		inputs.add(2.25);
+//
+//		ArrayList<Double> expectedOutput = new ArrayList<Double>();
+//		expectedOutput.add(.8);
+//		expectedOutput.add(.3);
+//
+//		FeedForwardANN net = new FeedForwardANN(2, 20, inputs, expectedOutput, false, false);
+//
+//		net.train();
+//		net.print();
+//	}
+	
+//	@Test
+//	public void testLinearActivationSingle() {
+//		ArrayList<Double> inputs = new ArrayList<Double>();
+//		inputs.add(3.0);
+//		inputs.add(-0.89);
+//		inputs.add(.25);
+//
+//		ArrayList<Double> expectedOutput = new ArrayList<Double>();
+//		expectedOutput.add(.55);
+//
+//		FeedForwardANN net = new FeedForwardANN(2, 20, inputs, expectedOutput, false, false);
+//
+//		net.train();
+//		net.print();
+//	}
+	
+//	@Test
+//	public void testTrainSingleOutputMomentum() {
+//		ArrayList<Double> inputs = new ArrayList<Double>();
+//		inputs.add(3.0);
+//		inputs.add(-0.89);
+//		inputs.add(.25);
+//
+//		ArrayList<Double> expectedOutput = new ArrayList<Double>();
+//		expectedOutput.add(.999);
+//
+//		FeedForwardANN net = new FeedForwardANN(1, 2, inputs, expectedOutput, true, true);
+//
+//		net.train();
+//		net.print();
+//	}
+	
+//	@Test
+//	public void testTrainMultOutputMomentum() {
+//		ArrayList<Double> inputs = new ArrayList<Double>();
+//		inputs.add(3.0);
+//		inputs.add(-0.89);
+//		inputs.add(.25);
+//
+//		ArrayList<Double> expectedOutput = new ArrayList<Double>();
+//		expectedOutput.add(.972);
+//		expectedOutput.add(.555);
+//
+//		FeedForwardANN net = new FeedForwardANN(2, 10, inputs, expectedOutput, true, true);
+//
+//		net.train();
+//		net.print();
+//	}
 	
 	@Test
-	public void backPropTestMoreThanOne(){
-		FeedForwardANN net = new FeedForwardANN(3);
+	public void testRBSingleOutput1() {
 		ArrayList<Double> inputs = new ArrayList<Double>();
-		inputs.add(-2.5);
-		inputs.add(2.0);
-		inputs.add(-1.0);
 		inputs.add(3.0);
-		
-		ArrayList<Double> expectedOutputs = new ArrayList<Double>();
-		expectedOutputs.add(5.0);
-		expectedOutputs.add(1.0);
-		expectedOutputs.add(6.2);
-		expectedOutputs.add(1.0);
-		
-		net.setExpectedOutputs(expectedOutputs);
-		net.generateOutput(inputs);	
-		net.backProp();
+		inputs.add(2.0);
+		inputs.add(.25);
 
-		for (int i = 0; i < 10; i++){
-			net.generateOutput(inputs);	
-			net.backProp();
-		}
+		ArrayList<Double> expectedOutput = new ArrayList<Double>();
+		expectedOutput.add(.999);
+
+		FeedForwardANN net = new FeedForwardANN(1, 2, inputs, expectedOutput, true, false);
+
+		net.train();
+		net.print();
 	} 
+	
+//	@Test
+//	public void testRBSingleOutput2() {
+//		ArrayList<Double> inputs = new ArrayList<Double>();
+//		inputs.add(7.0);
+//		inputs.add(-0.84);
+//		inputs.add(.05);
+//
+//		ArrayList<Double> expectedOutput = new ArrayList<Double>();
+//		expectedOutput.add(.25);
+//
+//		FeedForwardANN net = new FeedForwardANN(1, 2, inputs, expectedOutput, true, false);
+//
+//		net.train();
+//		net.print();
+//	}
 
 }
