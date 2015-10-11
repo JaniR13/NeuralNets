df9034838703b8e60e707372541a9f859da6e568
Janette <janette.rounds@msu.montana.edu>
2015-10-11 17:14:33 -0600

    Added Rollie's things

--- NeuralNets/src/runmodels/MeanSquaredError.java
+++ NeuralNets/src/runmodels/MeanSquaredError.java
@@ -1,59 +1,19 @@
 package runmodels;
 
-import java.util.ArrayList;
-
-public class MeanSquaredError extends AbstractFunction {
-	// TODO: will we even need this?
+public class MeanSquaredError {
 	
-	public MeanSquaredError(){
-		//does nothing
-	}
-	
-	@Override
-	double calcfx(double x) {
-		// distance from a single instance to itself is zero
-		return 0;
-	}
-
-	@Override
-	double calcderivfx(double x) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+	// TODO: add error for whole network
+	// also possibly constructor
 	
 	/**
-	 * Calculated mean squared error for a vector of outputs
-	 * @param predictions node/network outputs
-	 * @param expectedOutputs expected node/network output
-	 * @return error
-	 */
-	public double calcError(ArrayList<Double> predictions, ArrayList<Double> expectedOutputs){
-		int n = predictions.size();
-		double error = 0;
-		for (int i = 0; i < n; i++){
-			error += (predictions.get(i) - expectedOutputs.get(i))*(predictions.get(i) - expectedOutputs.get(i));
-		}	
-		return error/n;
-	}
-	
-
-	/** Calculates mean squared error for a single output
-	 * @param target expected node/network output
-	 * @param observedOutput observed node/network output
+	 * Calculates the derivative of error with respect to output
+	 * @param output the neuron's output
+	 * @param target the target output
 	 * @return
 	 */
-	public double calcError(double target, double observedOutput){	
-		return target - observedOutput;
-	}
-	
-	/**
-	 * Calculates partial derivative of error with respect to expected output
-	 * @param target expected node/network output
-	 * @param observedOutput observed node/network output
-	 * @return
-	 */
-	public double calcDerivError(double target, double observedOutput){	
-		return -1*(target - observedOutput);
+	public Double calcDerivwrtOutput(Double output, Double target){
+		//System.out.println("********** (" + target + " - " + output + ") **********");
+		return (output - target);
 	}
 
 }
