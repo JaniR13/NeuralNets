70c93c4135f36336a428e6341c01766e4b3bfdde
Janette <janette.rounds@msu.montana.edu>
2015-10-10 18:34:52 -0600

    Finished k-means and finished before test

--- NeuralNets/src/runmodels/RBFNode.java
+++ NeuralNets/src/runmodels/RBFNode.java
@@ -14,14 +14,13 @@ import java.util.ArrayList;
 public class RBFNode {
     ArrayList<Double> outweights; //weights to each output node from this RBF
     //ArrayList<Double> inweights; //Most RBF's don't have weights from input nodes to hidden nodes
-    double var;//need variance matrix. TODO how do I implement that?
     ArrayList<Double> means; //center of the cluster as vector
     double activationOut;//Activation output
     Distance dist = new Distance();
     public RBFNode(){
         
     }
-    public double calculateActivation(ArrayList<Double> x){//TODO make decision about variance matrix
+    public double calculateActivation(ArrayList<Double> x, double var){//TODO make decision about variance matrix
         double variance = 1/(2*Math.pow(var, 2));
         double error = Math.pow(dist.calculateDistance(x, means),2);
         return Math.exp(variance*error);
